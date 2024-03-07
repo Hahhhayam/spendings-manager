ea29b1a8d7774a2f812e10098744ff96998e2f90
peperevenge <resetlol2004@gmail.com>
2024-03-07 14:52:02 +0200

    enhancement #3: Add currency sign (acronym) support
    Desided to not store currency sign due to locale differences: different signs and their placements.
    - Currency changed
    - migration applied

---
+++ DAL/Migrations/20240307124654_currency-acronym.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class currencyacronym : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Acronym",
+                table: "Currencies",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Acronym",
+                table: "Currencies");
+        }
+    }
+}
