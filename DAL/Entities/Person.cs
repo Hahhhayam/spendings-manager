43b13632c0e14129814b33001720b7c21ed09d30
peperevenge <resetlol2004@gmail.com>
2024-03-15 11:14:58 +0200

    migrate #19 /1: Add index for forbidden personName

--- DAL/Entities/Person.cs
+++ DAL/Entities/Person.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using DAL.Entities.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Entities
 {
+    [Index(nameof(Name), IsUnique = true)]
     public class Person : BaseEntity
     {
         [Required]
